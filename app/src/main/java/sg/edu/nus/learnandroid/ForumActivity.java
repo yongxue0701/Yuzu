14e0680daea82a48f72ca7f632cdb596c6f6245b
Xu Wei <xuwei.blue@gmail.com>
2017-10-21 07:36:41 +0800

    build forum

--- app/src/main/java/sg/edu/nus/learnandroid/ForumActivity.java
+++ app/src/main/java/sg/edu/nus/learnandroid/ForumActivity.java
@@ -1,7 +1,9 @@
 package sg.edu.nus.learnandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ForumActivity extends Activity {
 
@@ -9,5 +11,10 @@ public class ForumActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forum);
+    }
+
+    public void onClick_GoToForumActivity(View view) {
+        Intent myIntent = new Intent(this, ForumActivity.class);
+        startActivity(myIntent);
     }
 }
