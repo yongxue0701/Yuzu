28372b5d803c75dd0f63e2348d0b6b8992902d68
Yongxue <yongxue0701@gmail.com>
2017-10-23 00:53:52 +0800

    add UI for gender popup window update profile edit

--- app/src/main/java/sg/edu/nus/learnandroid/ProfileEditRVAdapter.java
+++ app/src/main/java/sg/edu/nus/learnandroid/ProfileEditRVAdapter.java
@@ -4,16 +4,23 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.drawable.ColorDrawable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.percent.PercentRelativeLayout;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import java.util.List;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 /**
  * Created by Yongxue on 21/10/17.
@@ -34,13 +41,13 @@ public class ProfileEditRVAdapter extends RecyclerView.Adapter {
     public static class EditTextTypeViewHolder extends RecyclerView.ViewHolder {
 
         public TextView buttonNameEditTextTV;
-        public EditText inputContentET;
+        public EditText buttonNameContentET;
 
         public EditTextTypeViewHolder(View itemView) {
             super(itemView);
 
             this.buttonNameEditTextTV = (TextView) itemView.findViewById(R.id.profileEdit_edittext_buttonName_TV);
-            this.inputContentET = (EditText) itemView.findViewById(R.id.profileEdit_edittext_ET);
+            this.buttonNameContentET = (EditText) itemView.findViewById(R.id.profileEdit_edittext_ET);
         }
     }
 
@@ -126,7 +133,7 @@ public class ProfileEditRVAdapter extends RecyclerView.Adapter {
                         if (mCursor != null && mCursor.moveToFirst() && (mCursor.getCount() == 1)) {
                             do {
                                 String username = mCursor.getString(mCursor.getColumnIndex("username"));
-                                editTextTypeViewHolder.inputContentET.setText(username);
+                                editTextTypeViewHolder.buttonNameContentET.setText(username);
                             } while (mCursor.moveToNext());
                         }
 
@@ -140,7 +147,7 @@ public class ProfileEditRVAdapter extends RecyclerView.Adapter {
                         if (mCursor != null && mCursor.moveToFirst() && (mCursor.getCount() == 1)) {
                             do {
                                 String username = mCursor.getString(mCursor.getColumnIndex("email"));
-                                editTextTypeViewHolder.inputContentET.setText(username);
+                                editTextTypeViewHolder.buttonNameContentET.setText(username);
                             } while (mCursor.moveToNext());
                         }
 
@@ -155,12 +162,26 @@ public class ProfileEditRVAdapter extends RecyclerView.Adapter {
                     buttonWithTextTypeViewHolder.buttonNameButtonWithTextTV.setText(itemView.getViewName());
 
                     if (itemView.getViewName().equals("Gender")) {
+                        userAccountDB.open();
+                        Cursor mCursor = userAccountDB.getRecordByIsLogin(1);
 
+                        if (mCursor != null && mCursor.moveToFirst() && (mCursor.getCount() == 1)) {
+                            do {
+                                String username = mCursor.getString(mCursor.getColumnIndex("gender"));
+                                buttonWithTextTypeViewHolder.buttonNameContentTV.setText(username);
+                            } while (mCursor.moveToNext());
+                        }
+
+                        mCursor.close();
+                        userAccountDB.close();
                     }
 
-                    if (itemView.getViewName().equals("Birthday")) {
-
-                    }
+                    buttonWithTextTypeViewHolder.buttonWithTextPRL.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            initiatePopupWindow(view);
+                        }
+                    });
 
                     break;
 
@@ -211,5 +232,58 @@ public class ProfileEditRVAdapter extends RecyclerView.Adapter {
     @Override
     public int getItemCount() {
         return editProfileList.size();
+    }
+
+    private void initiatePopupWindow(View view) {
+
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(LAYOUT_INFLATER_SERVICE);
+        final View popupView = layoutInflater.inflate(R.layout.gender_popup, null);
+        final PopupWindow popupWindow = new PopupWindow(
+                popupView,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        popupWindow.showAtLocation(popupView, Gravity.BOTTOM, 0, 0);
+        popupWindow.setBackgroundDrawable(new ColorDrawable());
+        popupWindow.setFocusable(true);
+        popupWindow.setOutsideTouchable(true);
+        popupWindow.setTouchable(true);
+        popupWindow.update();
+
+        LinearLayout cancelLL = (LinearLayout) popupView.findViewById(R.id.cancel_textview_LL);
+        cancelLL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popupWindow.dismiss();
+            }
+        });
+
+        TextView genderPopupNullTV = (TextView) popupView.findViewById(R.id.gender_popup_null_TV);
+        genderPopupNullTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        TextView genderPopupMaleTV = (TextView) popupView.findViewById(R.id.gender_popup_male_TV);
+        genderPopupMaleTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        TextView genderPopupFemaleTV = (TextView) popupView.findViewById(R.id.gender_popup_female_TV);
+        genderPopupFemaleTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
