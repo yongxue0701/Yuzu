bb2330ecec5abf38e5b2509dd48fe3ae3985bd59
Xu Wei <xuwei.blue@gmail.com>
2017-10-26 20:24:35 +0800

    delete sahre button

--- app/src/main/java/sg/edu/nus/learnandroid/ForumActivity.java
+++ app/src/main/java/sg/edu/nus/learnandroid/ForumActivity.java
@@ -1,10 +1,7 @@
 package sg.edu.nus.learnandroid;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.facebook.AccessToken;
 import com.facebook.AccessTokenTracker;
@@ -17,30 +14,14 @@ public class ForumActivity extends Activity {
     private AccessToken accessToken;
     private final static String TAG = LoginActivity.class.getName().toString();
 
-    Button bt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forum);
-        bt = (Button) findViewById(R.id.button);
-        bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(Intent.ACTION_SEND);
-                myIntent.setType("text/plain");
-                String shareBody = "Your content here";
-                String shareSub = "Your subject here";
-                myIntent.putExtra(Intent.EXTRA_SUBJECT, shareSub);
-                myIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
-                startActivity(Intent.createChooser(myIntent, "Share your thoughts"));
-            }
 
-        });
     }
 
-    public void onClick_GoToForumActivity(View view) {
-        Intent myIntent = new Intent(this, ForumActivity.class);
-        startActivity(myIntent);
-    }
+
 }
