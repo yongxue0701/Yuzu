d457f48c95d3a9fd65532fbeac0e574a82feaec5
Yongxue <yongxue0701@gmail.com>
2017-11-02 02:41:33 +0800

    update fragment quiz

--- app/src/main/java/sg/edu/nus/learnandroid/FragmentQuizActivity.java
+++ app/src/main/java/sg/edu/nus/learnandroid/FragmentQuizActivity.java
@@ -1,13 +1,52 @@
 package sg.edu.nus.learnandroid;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class FragmentQuizActivity extends Activity {
+public class FragmentQuizActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_quiz);
+
+        // Set up custom action bar with back button
+        getSupportActionBar().setDisplayOptions(getActionBar().DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.action_bar_with_nextbtn);
+
+        // Set up the back button and title on action bar
+        View view = getSupportActionBar().getCustomView();
+
+        TextView actionBarTitleTV = (TextView) findViewById(R.id.action_bar_title_with_nextbtn);
+        actionBarTitleTV.setText("Fragments");
+
+        ImageView backBtnIV = (ImageView) findViewById(R.id.action_bar_back_with_nextbtn);
+        backBtnIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        TextView nextBtnTV = (TextView) findViewById(R.id.action_bar_next_with_nextbtn);
+        nextBtnTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getApplicationContext(), FragmentQuizActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
+        WebView webView = (WebView) findViewById(R.id.fragment_quiz_webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/www/fragment_quiz.html");
     }
 }
