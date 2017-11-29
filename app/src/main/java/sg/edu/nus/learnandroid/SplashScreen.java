03fc96fa0093c1a08010519e6b87da40f8bc86ce
Yongxue <yongxue0701@gmail.com>
2017-11-29 21:06:02 +0800

    update splash screen -if user is logined, after splash screen, course map will display -if user is un-logined, user has to login first

--- app/src/main/java/sg/edu/nus/learnandroid/SplashScreen.java
+++ app/src/main/java/sg/edu/nus/learnandroid/SplashScreen.java
@@ -2,18 +2,22 @@ package sg.edu.nus.learnandroid;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashScreen extends Activity {
 
-    // Splash screen timer
+    UserAccountDB userAccountDB;
+
     private static int SPLASH_TIME_OUT = 3500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        userAccountDB = new UserAccountDB(this);
 
         new Handler().postDelayed(new Runnable() {
 
@@ -24,12 +28,25 @@ public class SplashScreen extends Activity {
 
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashScreen.this, LoginActivity.class);
-                startActivity(i);
 
-                // close this activity
+                userAccountDB.open();
+                Cursor mCursor = userAccountDB.getRecordByIsLogin(1);
+
+                if (mCursor != null && mCursor.moveToFirst() && (mCursor.getCount() == 1)) {
+                    do {
+                        Intent i = new Intent(getApplicationContext(), CourseMapActivity.class);
+                        startActivity(i);
+                        overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
+                    } while (mCursor.moveToNext());
+                } else {
+                    Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(i);
+                    overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
+                }
+
+                mCursor.close();
+                userAccountDB.close();
+
                 finish();
             }
         }, SPLASH_TIME_OUT);
